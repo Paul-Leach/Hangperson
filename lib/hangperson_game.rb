131e51c8c9d7f210744142b448f3d4126a1aae16
Paul <pauljleach@hotmail.com>
2016-06-20 19:30:55 +0000

    make pending tests active; complete model

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -9,9 +9,47 @@ class HangpersonGame
   # end
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = "-" * @word.length
+    @bad_guess_count = 0
   end
 
+  attr_reader :guesses
+  attr_reader :wrong_guesses
+  attr_reader :word
+  attr_reader :word_with_guesses
+  
+  def guess(letter)
+    raise(ArgumentError) if letter == '' || letter == nil ||
+     (letter.length > 1) || (letter =~ /[a-zA-Z]/) != 0
+    
+    letter.downcase!
+    if @word.include? letter
+      if @guesses.include? letter
+        return false
+      end
+      @guesses += letter
+      #  every letter of @word not in @guesses to "-" and set to @word_with_guesses
+      p = Regexp.new("[^"+@guesses+"]")
+      @word_with_guesses = @word.gsub(p,'-')
+      return true
+    else
+      @bad_guess_count += 1
+      if @wrong_guesses.include? letter
+        return false
+      end
+      @wrong_guesses += letter
+      return true
+    end
+  end
+  
+  def check_win_or_lose
+    return :win if @word == @word_with_guesses
+    return :lose if @bad_guess_count >= 7
+    return :play
+  end
   def self.get_random_word
     require 'uri'
     require 'net/http'
